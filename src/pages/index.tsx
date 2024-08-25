330c0b20158b1833f78642746411669398b3a973
Italo Git <sulamita.italo12@gmail.com>
2024-08-25 20:46:17 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,53 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useForm } from 'react-hook-form';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  const { register, handleSubmit } = useForm()
+
+  function criarUsuario(data: any) {
+    console.log(data)
+  }
+
   return (
     <main
-      className={`h-screen bg-zinc-50 flex  ${inter.className}`}
+      className={`h-screen bg-slate-500 flex items-center justify-center ${inter.className}`}
     >
-      <form action="" className="flex felx-col gap-4">
-        <label htmlFor="">E-mail:</label>
-        <input type="email" name="email"/>
+      <form 
+        onSubmit={handleSubmit(criarUsuario)} 
+        className="flex flex-col gap-4 w-full max-w-xs"
+      >
+        <div className="flex flex-col gap-1">
 
-        <label htmlFor="">Senha:</label>
-        <input type="password" name="password" />
+          <label htmlFor="email">
+            E-mail:
+          </label>
+          <input
+            type="email"
+            className="border border-zinc-200 shadow-sm rounded px-3"
+            {...register('email')}
+          />
+  
+          <label htmlFor="password">
+            Senha:
+          </label>
+          <input 
+            type="password" 
+            className="border border-zinc-200 shadow-sm rounded px-3"
+            {...register('password')}
+          />
+  
+          <button
+            type="submit"
+            className="bg-emerald-500 rounded font-semibold text-white h-8 hover:bg-emerald-700 transition-colors"
+          >
+            Salvar
+          </button>
 
-        <button type="submit">Submit</button>
+        </div>
       </form>
     </main>
   );
